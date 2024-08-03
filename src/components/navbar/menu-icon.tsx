1ec9b49f091255718bb3f5ed3cdfbf1aa362a9fa
othmane bakkass <othmanebakkass@gmail.com>
2024-08-03 18:31:54 +0100

    feat: add more nav links

--- src/components/navbar/menu-icon.tsx
+++ src/components/navbar/menu-icon.tsx
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function MenuIcon({
 	class: classes,
 	onClick,
-	...props
 }: {
 	class?: string;
 	onClick?: () => void;
--- src/components/navbar/menu.tsx
+++ src/components/navbar/menu.tsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MenuIcon } from "./menu-icon.tsx";
+import { useMediaQuery } from "usehooks-ts";
 
 export function Menu(props: { menuLink?: any[] }) {
 	const [show, setShow] = useState(false);
+	const matches = useMediaQuery("(min-width: 641px)");
+	useEffect(() => {
+		if (matches) {
+			setShow(false);
+		}
+	}, [matches]);
 	return (
 		<>
 			<ul
 				aria-hidden={show}
 				data-show={show}
-				className="max-[640px]:data-[show=false]:pointer-events-none max-[640px]:data-[show=false]:opacity-0 transition-opacity data-[show=false]:duration-300  flex flex-col sm:flex-row max-[640px]:w-full max-[640px]:rounded-lg max-[640px]:p-4 max-[640px]:absolute top-full max-[640px]:bg-background-100/80 max-[640px]:backdrop-blur-sm gap-x-2 max-[640px]:border max-[640px]:border-background-200"
-			>
+				className=" max-[640px]:data-[show=false]:pointer-events-none max-[640px]:data-[show=false]:opacity-0 max-[640px]:data-[show=false]:transition-opacity max-[640px]:data-[show=true]:duration-300  flex flex-col sm:flex-row max-[640px]:w-full max-[640px]:rounded-lg max-[640px]:p-4 max-[640px]:absolute top-full max-[640px]:bg-background-100/80 max-[640px]:backdrop-blur-sm gap-x-2 max-[640px]:border max-[640px]:data-[show=true]:z-50 max-[640px]:border-background-200">
 				{props.menuLink}
 			</ul>
 			<MenuIcon
 				onClick={() => {
-					console.log("🚀 ~ hello ~");
-
 					setShow(!show);
 				}}
 			/>
